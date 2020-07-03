9e964377b7da9450a8d26594d8d4ed0566cf5b8c
Arslan <rslnkrmt2552@gmail.com>
2020-07-03 19:06:43 +0300

    Add weatherbit api and multiapi

--- main.go
+++ main.go
@@ -18,20 +18,37 @@ type weatherData struct {
 	} `json:"main"`
 }
 
+type weatherProvider interface {
+	temperature(city string) (float64, error)
+}
+
+type openWeatherMap struct {
+	url string
+}
+
+type weatherBit struct {
+	url string
+}
+
+type multiWeatherProvider []weatherProvider
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	APIKEY := os.Getenv("APIKEY")
 
-	APIURL := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?APPID=%s&q=", APIKEY)
+	mw := multiWeatherProvider{
+		openWeatherMap{
+			url: fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?APPID=%s&q=", os.Getenv("OPENWEATHER"))},
+		weatherBit{url: fmt.Sprintf("https://api.weatherbit.io/v2.0/current?key=%s&city=", os.Getenv("WEATHERBIT"))},
+	}
 
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
 		city := strings.SplitN(r.URL.Path, "/", 3)[2]
 
-		data, err := query(APIURL, city)
+		data, err := mw[1].temperature(city)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -49,8 +66,8 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello!"))
 }
 
-func query(url string, city string) (weatherData, error) {
-	resp, err := http.Get(url + city)
+func query(url, city string) (weatherData, error) {
+	resp, err := http.Get(city)
 
 	if err != nil {
 		return weatherData{}, err
@@ -65,4 +82,81 @@ func query(url string, city string) (weatherData, error) {
 	}
 
 	return d, nil
+}
+
+func (w openWeatherMap) temperature(city string) (float64, error) {
+	resp, err := http.Get(w.url + city)
+
+	if err != nil {
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+
+	var d struct {
+		Main struct {
+			Kelvin float64 `json:"temp"`
+		} `json:"main"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return 0, err
+	}
+
+	log.Printf("openWeatherMap: %s: %.2f", city, d.Main.Kelvin)
+
+	return d.Main.Kelvin, nil
+}
+
+func (w weatherBit) temperature(city string) (float64, error) {
+	resp, err := http.Get(w.url + city)
+	if err != nil {
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+
+	var d struct {
+		Data []struct {
+			Celsius float64 `json:"temp"`
+		} `json:"data"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return 0, err
+	}
+
+	kelvin := d.Data[0].Celsius + 273.15
+	log.Printf("weatherBit: %s: %.2f", city, kelvin)
+	return kelvin, nil
+}
+
+func temperature(city string, providers ...weatherProvider) (float64, error) {
+	sum := 0.0
+	for _, provider := range providers {
+		k, err := provider.temperature(city)
+		if err != nil {
+			return 0, nil
+		}
+
+		sum += k
+	}
+
+	return sum / float64(len(providers)), nil
+}
+
+func (w multiWeatherProvider) temperature(city string) (float64, error) {
+	sum := 0.0
+
+	for _, provider := range w {
+		k, err := provider.temperature(city)
+
+		if err != nil {
+			return 0, err
+		}
+
+		sum += k
+	}
+
+	return sum / float64(len(w)), nil
 }
