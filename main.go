981a496a64853158687f8697db5bec40a66c3f46
Arslan <rslnkrmt2552@gmail.com>
2020-07-03 18:22:23 +0300

    Add basic api query

--- main.go
+++ main.go
@@ -1,12 +1,68 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
 
 func main() {
-	http.HandleFunc("/", hello)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	APIKEY := os.Getenv("APIKEY")
+
+	APIURL := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?APPID=%s&q=", APIKEY)
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
+
+		data, err := query(APIURL, city)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(data)
+
+	})
 	http.ListenAndServe(":8080", nil)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello!"))
+}
+
+func query(url string, city string) (weatherData, error) {
+	resp, err := http.Get(url + city)
+
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+
+	return d, nil
 }
